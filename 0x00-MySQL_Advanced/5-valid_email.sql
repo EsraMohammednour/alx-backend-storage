54d035f8a3e209a761fbe99cca48a088e8abc737
esra <esraa2023112@gmail.com>
2024-10-17 20:59:42 +0300

    done

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,13 +1,12 @@
 -- SQL script that creates a trigger that resets the attribute
 DROP TRIGGER IF EXISTS reset;
 DELIMITER ^^
-
 CREATE TRIGGER reset
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
     IF OLD.email != NEW.email THEN
-        SET NEW.valid_email = 1
+        SET NEW.valid_email = 1;
     ELSE
 	SET NEW.valid_email = NEW.valid_email;
     END IF;
