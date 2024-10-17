983cd718b0fa04214b312c07abeac34173c97377
esra <esraa2023112@gmail.com>
2024-10-17 19:08:39 +0300

    done

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,15 @@
+-- SQL script that creates a trigger that resets the attribute
+DROP TRIGGER IF EXISTS reset;
+DELIMITER ^^
+
+CREATE TRIGGER reset
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+        SET NEW.valid_email = 1;
+    END IF;
+END;
+^^
+
+DELIMITER ;
