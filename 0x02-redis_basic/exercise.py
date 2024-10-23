a4147a4ebe300909e4f04733352c2931159f12c9
esra <esraa2023112@gmail.com>
2024-10-23 15:59:48 +0300

    done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,8 @@
 '''exercise.py'''
 import redis
 import uuid
-from typing import Union
+from typing import Union, callable
+
 
 class Cache:
     '''Clas that init value to store client'''
@@ -10,8 +11,24 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
         self.data = None
+
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''method that takes a data argument and returns a string'''
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: callable = None):
+        '''get function that take str value'''
+        value = self.data.get(key)
+        if value is not None and fn is not None:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str):
+        '''function that return str value'''
+        return self.get(key, str)
+
+    def get_int(self, key: str):
+        '''function that return int value'''
+        return self.get(key, int)
