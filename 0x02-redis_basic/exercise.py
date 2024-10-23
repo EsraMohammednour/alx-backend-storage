2af42bf2b648498cdca79f4c6a176ff70b4b821e
esra <esraa2023112@gmail.com>
2024-10-23 15:08:26 +0300

    done

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''exercise.py'''
+import redis
+import uuid
+
+class Cache:
+    '''Clas that init value to store client'''
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+
+    def store(self, data):
+        self.data = data
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
