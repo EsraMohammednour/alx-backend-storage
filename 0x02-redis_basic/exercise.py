f6c63d9926019c447bcadcfc444ca033b1cf0994
esra <esraa2023112@gmail.com>
2024-10-23 15:12:29 +0300

    done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,12 +3,12 @@
 import redis
 import uuid
 
+
 class Cache:
     '''Clas that init value to store client'''
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
-
 
     def store(self, data):
         '''method that takes a data argument and returns a string'''
